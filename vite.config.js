44332323d8107ba82b3ee46b7092e2cc0de931b6
Dannygua <Danny.guanuna@epn.edu.ec>
2023-10-02 13:24:21 -0500

    d

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Dannygua.github.io/vite-deploy",
+  base: "https://Dannygua.github.io",
 });
